dd4a1094248c95bd41e231e36498a201fef69628
Ben Fulcher <ben.d.fulcher@gmail.com>
2019-08-23 16:48:12 +1000

    Working up for lecture

--- wordFreq.m
+++ wordFreq.m
@@ -1,46 +1,47 @@
-clc
-clear
 
-disp('Reading the bible')
+fprintf(1,'Reading the bible...');
 
 fid = fopen('Bible.txt');
 % Read all lines & collect in cell array
-txt = textscan(fid,'%s','delimiter','\n'); 
+txt = textscan(fid,'%s','delimiter','\n');
+fprintf(1,' Done. Was I fast? How long does it usually take you?\n');
 % Convert string to numerical value
 str = txt{1} ;
- % lower all text
+% All text to lower case:
 str = lower(str);
-disp('I am enlightened')
+fprintf(1,'I am enlightened\n');
 
-disp('How frequently did I see each word?')
- 
- alldata = str;
- buffer = [alldata(:), repmat( {' '}, numel( alldata ), 1 )]' ;
- buffer = strtrim( [buffer{:}] ) ;
- 
- 
- % Split giant string on white spaces, commas and fullstips
- words = strsplit( buffer, {' ',', ','.'} )' ;
- [words_u, ~, idxU] = unique( words ) ;
- counts = accumarray( idxU, 1 ) ;
- 
- % - Sort entries by count.
- [~, idxS] = sort( counts, 'descend' ) ;
- words_us = words_u(idxS) ;
- counts_s = counts(idxS) ;
- % - Build cell array of unique words and counts.
- result = [words_us, num2cell( counts_s )] ;
- 
- xvals = 1:numel(words_us);
+% Count unique words:
+alldata = str;
+buffer = [alldata(:), repmat( {' '}, numel( alldata ), 1 )]';
+buffer = strtrim([buffer{:}]);
+
+% Split giant string on white spaces, commas and fullstips
+words = strsplit( buffer, {' ',', ','.'} )';
+[words_u, ~, idxU] = unique( words );
+counts = accumarray(idxU,1);
+
+fprintf(1,'I found %u different words in the bible\n',length(counts));
+
+% - Sort entries by count.
+[~, idxS] = sort(counts, 'descend');
+words_us = words_u(idxS);
+counts_s = counts(idxS);
+% - Build cell array of unique words and counts.
+result = [words_us, num2cell(counts_s)] ;
+
+%-------------------------------------------------------------------------------
+% PLOT:
+f = figure('color','w');
+xvals = 1:numel(words_us);
 loglog(xvals,counts_s,'x')
 xlabel('Word rank')
 ylabel('Frequency')
 set(gca,'FontSize', 18)
 
 %pause
-
 hold on
-p= polyfit(log10(xvals'),log10(counts_s),1);
+p = polyfit(log10(xvals'),log10(counts_s),1);
 yvals = (10.^p(2)).*xvals.^p(1);
 
 plot(xvals,yvals,'-r')
